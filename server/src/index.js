ecd0fb93f45942e7caed3df7d6fdd8e18a83524f
Jacobo Cortina <jacobcg@gmail.com>
2016-11-06 20:02:01 +0100

    added thinky, basic database connections, user and question schemas

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,19 @@
 // our packages
 import app from './app';
 import {logger} from './util';
-// start server
-app.listen(8080, function() {
-  const host = this.address().address;
-  const port = this.address().port;
-  logger.info(`Experts server is listening at http://${host}:${port}`);
-});
+import {thinky} from './db';
 
+// wait for DB to initialize
+thinky.dbReady().then(() => {
+  logger.info('Database ready, starting server...');
+
+  // start server
+  app.listen(8080, function() {
+    const host = this.address().address;
+    const port = this.address().port;
+    logger.info(`Experts server is listening at http://${host}:${port}`);
+  });
+});
 // output all uncaught exceptions
 process.on('uncaughtException', err => logger.error('uncaught exception:', err));
 process.on('unhandledRejection', error => logger.error('unhandled rejection:', error));
