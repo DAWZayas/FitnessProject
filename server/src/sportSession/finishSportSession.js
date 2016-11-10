51e20c6bafe7ee543397a79f3dea49cb6f960594
Jacobo Cortina <jacobcg@gmail.com>
2016-11-10 22:07:15 +0100

    fixed error in finishedSportSession when updating the endTime

--- server/src/sportSession/finishSportSession.js
+++ server/src/sportSession/finishSportSession.js
@@ -1,15 +1,17 @@
 import {SportSession} from '../db';
 import {asyncRequest} from '../util';
+import {thinky} from '../db/thinky';
 
 export default (app) => {
   app.post('/api/sportSession/:id', asyncRequest(async (req, res) => {
     const sportSession = await SportSession.get(req.params.id);
-    sportSession.distance = 100;
+    sportSession.endTime = thinky.r.now();
     try {
       await sportSession.save();
     } catch (e) {
       res.status(400).send({error: e.toString()});
       return;
     }
+    res.send(sportSession);
   }));
 };
