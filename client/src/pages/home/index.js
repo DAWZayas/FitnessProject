132c671d9cecf0b894e6734a0ec01d5edb3d30a7
Jacobo Cortina <jacobcg@gmail.com>
2017-02-06 23:17:21 +0100

    improved styles of home and stats page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 import {connect} from 'react-redux';
+import Chart from 'chart.js'
 
 const mapStateToProps = (state) => ({
 });
@@ -10,32 +11,40 @@ const mapDispatchToProps = (dispatch) => ({
 
 const Home = () => (
   <div className="jumbotron animated fadeIn">
-
     <div className="card">
       <div className="card-block">
+        <Link to="/sessions" className="float-xs-right">
+          <img className="rounded z-depth-1" src="http://localhost:8080/static/images/exercises/04.png" width="100px" height="100px" alt="Generic" />
+        </Link>
         <h4 className="card-title">Sport session</h4>
         <Link to="/sessions" className="btn btn-default">Go!</Link>
       </div>
     </div>
-
     <div className="card">
       <div className="card-block">
+        <Link to="/routine" className="float-xs-right">
+          <img className="rounded z-depth-1" src="http://localhost:8080/static/images/exercises/04.png" width="100px" height="100px" alt="Generic" />
+        </Link>
         <h4 className="card-title">Exercise routine</h4>
         <Link to="/routine" className="btn btn-default">Go!</Link>
       </div>
     </div>
-
     <div className="card">
       <div className="card-block">
+        <Link to="/stats" className="float-xs-right">
+          <img className="rounded z-depth-1" src="http://localhost:8080/static/images/exercises/04.png" width="100px" height="100px" alt="Generic" />
+        </Link>
         <h4 className="card-title">Exercise statistics</h4>
         <Link to="/stats" className="btn btn-default">Go!</Link>
       </div>
     </div>
-
     <div className="card">
       <div className="card-block">
+        <Link to="/atlethes" className="float-xs-right">
+          <img className="rounded z-depth-1" src="http://localhost:8080/static/images/exercises/04.png" width="100px" height="100px" alt="Generic" />
+        </Link>
         <h4 className="card-title">Athletes nearby</h4>
-        <Link to="/athletes" className="btn btn-default">Go!</Link>
+        <Link to="/atlethes" className="btn btn-default">Go!</Link>
       </div>
     </div>
   </div>
--- client/src/pages/stats/index.js
+++ client/src/pages/stats/index.js
@@ -1,17 +1,100 @@
-import React from 'react';
-import {Link} from 'react-router';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router';
+import InfiniteScroll from 'redux-infinite-scroll';
 
-const mapStateToProps = (state) => ({
+import {retrieveStatsData} from '../../store/actions';
+import {server as serverConfig} from '../../../config';
+
+import Loader from '../../components/loader';
+
+const mapDispatchToProps = dispatch => ({
+  fetchStatsData: payload => dispatch(retrieveStatsData(payload)),
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapStateToProps = state => ({
+  user: state.auth.user.id,
 });
 
-const Stats = () => (
-  <div className="jumbotron animated fadeInRight">
-    <h1>Stats page</h1>
-  </div>
-);
+const style = {
+  navbarNav: {
+    width: '100%',
+    textAlign: 'center',
+  },
+  li: {
+    float: 'none',
+    display: 'inline-block',
+  },
+};
 
+class Stats extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: 'Info',
+      filterText: '',
+      showMine: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  // componentWillMount() {
+  //   this.props.fetchRoutines();
+  // }
+
+  // componentDidMount() {
+  //   if (this.state.round === this.state.routine.rounds) {
+  //     this.setState({round: 'FINAL'});
+  //   }
+  // }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.setState(
+      {
+        tab: e.target.textContent,
+      }
+    );
+    this.props.fetchStatsData(
+      {
+        type: this.state.tab,
+        userId: this.props.user,
+        actualDate: new Date(),
+      });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="card text-xs-center">
+          <h1>Stats</h1>
+        </div>
+        <nav className="navbar navbar-dark default-color">
+          <ul className="nav navbar-nav" style={style.navbarNav}>
+            <li className={`nav-item ${this.state.tab === 'Info' && 'active'}`} style={style.li}>
+              <a href="#2" className="nav-link" onClick={this.handleClick}>Info</a>
+            </li>
+            <li className={`nav-item ${this.state.tab === 'Running' && 'active'}`} style={style.li}>
+              <a href="#2" className="nav-link" onClick={this.handleClick}>Running</a>
+            </li>
+            <li className={`nav-item ${this.state.tab === 'Cycling' && 'active'}`} style={style.li}>
+              <a href="#2" className="nav-link" onClick={this.handleClick}>Cycling</a>
+            </li>
+            <li className={`nav-item ${this.state.tab === 'Walking' && 'active'}`} style={style.li}>
+              <a href="#2" className="nav-link" onClick={this.handleClick}>Walking</a>
+            </li>
+            <li className={`nav-item ${this.state.tab === 'Routines' && 'active'}`} style={style.li}>
+              <a href="#2" className="nav-link" onClick={this.handleClick}>Routines</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
 export default connect(mapStateToProps, mapDispatchToProps)(Stats);
