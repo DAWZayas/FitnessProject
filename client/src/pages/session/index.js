634d316ebbcf272f0d9064e8e5df5672d37172f8
Jacobo Cortina <jacobcg@gmail.com>
2017-02-18 22:59:52 +0100

    fixed problem when not activate gps. Refactored session page

--- client/src/pages/session/index.js
+++ client/src/pages/session/index.js
@@ -5,20 +5,21 @@ import {connect} from 'react-redux';
 import {CountDown, Timer, Distance, SummarySession} from '../../components/session';
 import {finishSession, startSession} from '../../store/actions';
 
-const COUNTDOWN_TIME = 3;
+const COUNTDOWN_TIME = 5;
 
 const mapStateToProps = (state) => ({
   sessionState: state.session.state,
   sessionId: state.session.id,
   sport: state.session.sport,
   user: state.auth.user.login,
+  position: state.session.position,
 });
 
 const mapDispatchToProps = dispatch => ({
   onFinishSession: (payload) => dispatch(finishSession(payload)),
 });
 
-const started = (onFinishSession, sessionId, sport) => {
+const Started = ({onFinishSession, sessionId, sport, position}) => {
   let totalDistance = 0;
   const handleClick = (e) => {
     e.preventDefault();
@@ -32,7 +33,7 @@ const started = (onFinishSession, sessionId, sport) => {
       <div className="card card-block text-xs-center light-gradient">
         <Timer />
       </div>
-      <Distance totalDistance={distanceFunction} />
+      {position ? <Distance totalDistance={distanceFunction} /> : null}
       <div className="card card-block text-xs-center light-gradient">
         <button type="submit" className="btn btn-deep-orange btn-block" onClick={handleClick}>Finish</button>
       </div>
@@ -40,14 +41,15 @@ const started = (onFinishSession, sessionId, sport) => {
   );
 };
 
-const Session = ({sessionState, onFinishSession, sessionId, user, sport}) => (
+const Session = ({sessionState, onFinishSession, sessionId, user, sport, position}) => (
   <div className="jumbotron animated fadeIn grey lighten-5">
     {sessionState === 1 ?
       <div>
         <h1 className="card card-block text-xs-center">Session starts in...</h1>
         <CountDown time={COUNTDOWN_TIME} action={startSession} data={{user, sport}} />
       </div> : ''}
-    {sessionState === 2 ? started(onFinishSession, sessionId, sport) : ''}
+    {sessionState === 2 ?
+      <Started onFinishSession={onFinishSession} sessionId={sessionId} sport={sport} position={position} /> : ''}
     {sessionState === 3 ? <SummarySession /> : ''}
   </div>
 );
--- server/src/sportSession/addSportSession.js
+++ server/src/sportSession/addSportSession.js
@@ -4,8 +4,12 @@ import {SportSession} from '../db';
 export default (app) => {
   app.post('/api/sportSession', asyncRequest(async (req, res) => {
     // get user input
-    const {user, sport, startDate, pos} = req.body;
+    const {user, sport, startDate} = req.body;
+    let {pos} = req.body;
     // save new user
+    if (pos === 'undefined') {
+      pos = '{}';
+    }
     const sportSession = new SportSession({
       user,
       sport,
